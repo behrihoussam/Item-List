16a478dc57f8f056118fa4d6766305252ecfab2c
behrihoussam <45970611+behrihoussam@users.noreply.github.com>
2022-04-10 05:08:01 +0000

    Update Darktheme.js

--- Darktheme.js
+++ Darktheme.js
@@ -1,15 +1,17 @@
 const switchTheme = () => {
-    if (body.classList.contains("light_theme")
-        && modal.classList.contains("items_lightTheme")
-        && itemContainer.classList.contains("items_lightTheme")
-        && closeMenu.classList.contains("items_darkTheme")
-        && addItem.classList.contains("items_darkTheme")) {
+    if (body.classList.contains("light_theme")) {
 
         body.classList.remove("light_theme");
         body.classList.add("dark_theme");
 
         modal.classList.remove("items_lightTheme");
         modal.classList.add("items_darkTheme");
+
+        theme.classList.remove("items_lightTheme");
+        theme.classList.add("items_darkTheme");
+
+        openModal.classList.remove("items_lightTheme");
+        openModal.classList.add("items_darkTheme");
 
         itemContainer.classList.remove("items_lightTheme");
         itemContainer.classList.add("items_darkTheme");
@@ -20,17 +22,19 @@ const switchTheme = () => {
         addItem.classList.remove("items_darkTheme");
         addItem.classList.add("items_lightTheme");
 
-    } else if (body.classList.contains("dark_theme")
-        && modal.classList.contains("items_darkTheme")
-        && itemContainer.classList.contains("items_darkTheme")
-        && closeMenu.classList.contains("items_lightTheme")
-        && addItem.classList.contains("items_lightTheme")) {
+    } else if (body.classList.contains("dark_theme")) {
 
         body.classList.remove("dark_theme");
         body.classList.add("light_theme");
 
         modal.classList.remove("items_darkTheme");
         modal.classList.add("items_lightTheme");
+
+        theme.classList.remove("items_darkTheme");
+        theme.classList.add("items_lightTheme");
+
+        openModal.classList.remove("items_darkTheme");
+        openModal.classList.add("items_lightTheme");
 
         itemContainer.classList.remove("items_darkTheme");
         itemContainer.classList.add("items_lightTheme");
@@ -43,9 +47,3 @@ const switchTheme = () => {
     }
 }
 theme.addEventListener("click", switchTheme);
-/* 
-.light_theme
-.dark_theme
-.lightTheme
-.darkTheme 
-*/
